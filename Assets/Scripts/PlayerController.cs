18caf43fd5554bf28300526a6907a5f92c26de86
Cem Erkan Kilinc <cemkilinc@gmail.com>
2023-06-04 23:45:28 +0300

    AddTorque for flips, linear drag adjusted according to surface effector speed, angular drag adjusted according to torque

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 1f;
+    Rigidbody2D rbSnowboarder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbSnowboarder = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rbSnowboarder.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rbSnowboarder.AddTorque(-torqueAmount);
+        }
+    }
+}
